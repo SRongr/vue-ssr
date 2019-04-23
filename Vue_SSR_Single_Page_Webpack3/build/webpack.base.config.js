15561f33635f305710821cc6bab9407827236661
wangrongshuo <wrs970808@163.com>
2019-04-23 16:46:17 +0800

    add uglifyjs

--- Vue_SSR_Single_Page_Webpack3/build/webpack.base.config.js
+++ Vue_SSR_Single_Page_Webpack3/build/webpack.base.config.js
@@ -1,73 +1,98 @@
 var path = require('path');
 var webpack = require('webpack');
-
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const { VueLoaderPlugin } = require('vue-loader')
+const isProd = process.env.NODE_ENV === 'production'
 module.exports = {
-    output: {
-        path: path.resolve(__dirname, `../dist/`),
-        publicPath: '/dist/',       //发布后在线访问的url
-        filename: `[name].[hash:8].js`   //'[name].[chunkhash].js', '[name].[hash:8].js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    'vue-style-loader',
-                    'css-loader'
-                ],
-            }, {
-                test: /\.vue$/,
-                loader: 'vue-loader'
-            },
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/
-            },
-            {
-                test: /\.(png|jpg|gif|svg)$/,
-                loader: 'file-loader',
-                options: {
-                    name: 'img/[name].[hash:8].[ext]'    //自动hash命名图片等资源，并修改路径。路径需要根据项目实际情况确定。语法参考：https://doc.webpack-china.org/loaders/file-loader/
-                }
-            }
-        ]
-    },
-    resolve: {
-        alias: {
-            'vue$': 'vue/dist/vue.esm.js'
+  output: {
+    path: path.resolve(__dirname, `../dist/`),
+    publicPath: '/dist/',       //发布后在线访问的url
+    filename: `[name].[hash:8].js`   //'[name].[chunkhash].js', '[name].[hash:8].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ],
+      }, {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        // loader: 'babel-loader',
+        // exclude: /node_modules/
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
         },
-        extensions: ['*', '.js', '.vue', '.json']
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: 'img/[name].[hash:8].[ext]'    //自动hash命名图片等资源，并修改路径。路径需要根据项目实际情况确定。语法参考：https://doc.webpack-china.org/loaders/file-loader/
+        }
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
     },
-    performance: {
-        hints: false
-    },
-    devtool: '#eval-source-map'
+    extensions: ['*', '.js', '.vue', '.json']
+  },
+  performance: {
+    hints: false
+  },
+  plugins: isProd ? [
+    new UglifyJsPlugin({
+      uglifyOptions: {
+        compress: {
+          warnings: false,
+          drop_console: true, // 打包后去除console.log
+          collapse_vars: true, // 内嵌定义了但是只用到一次的变量
+          reduce_vars: true, // 提取出出现多次但是没有定义成变量去引用的静态值
+          pure_funcs: ['console.log']
+        }
+      },
+      sourceMap: false,
+      parallel: true // 使用多进程并行运行来提高构建速度
+    })
+  ] : [
+    new VueLoaderPlugin()
+  ],
+  devtool: '#eval-source-map'
 };
 
 if (process.env.NODE_ENV === 'production') {
-    module.exports.devtool = '#source-map'
-    // http://vue-loader.vuejs.org/en/workflow/production.html
-    module.exports.plugins = (module.exports.plugins || []).concat([
-        new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: '"production"'
-            }
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-            //sourceMap: true,  //开启max_line_len后会有报错，二选一
-            compress: {
-                warnings: false,
-                drop_debugger: true,
-                drop_console: true,
-                pure_funcs: ['alert']       //去除相应的函数
-            },
-            output: {
-                max_line_len: 100
-            }
-        }),
-        new webpack.LoaderOptionsPlugin({
-            minimize: true
-        })
-    ]);
+  module.exports.devtool = '#source-map'
+  // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      //sourceMap: true,  //开启max_line_len后会有报错，二选一
+      compress: {
+        warnings: false,
+        drop_debugger: true,
+        drop_console: true,
+        pure_funcs: ['alert']       //去除相应的函数
+      },
+      output: {
+        max_line_len: 100
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    })
+  ]);
 }
